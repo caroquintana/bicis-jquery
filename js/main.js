e35ca3594fe2d46be52eae2e7e11817a6467876f
caroquintana <caro.quintana.n@gmail.com>
2017-07-04 16:27:48 -0400

    corrigiendo indentacion

--- js/main.js
+++ js/main.js
@@ -1,47 +1,45 @@
 $(document).ready(function(){
 
-$('.submit-btn').click(function(){
-    validateForm();   
-});
+    $('.submit-btn').click(function(){
+        validateForm();   
+    });
 
-function validateForm(){
+    function validateForm(){
 
+        var nameReg = /^[A-Za-z]+$/;
+        var emailReg = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
 
-    var nameReg = /^[A-Za-z]+$/;
-    var emailReg = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
-    var largenumber = ($('#input-password').val()).length >= 6;
+        var names = $('#name').val();
+        var lastname = $('#lastname').val();
+        var email = $('#input-email').val();
+        var password = $('#input-password').val();
 
-    var names = $('#name').val();
-    var lastname = $('#lastname').val();
-    var email = $('#input-email').val();
-    var password = $('#input-password').val();
+        var inputVal = new Array(names, lastname, email, password);
 
-    var inputVal = new Array(names, lastname, email, password);
+        var inputMessage = new Array("nombre", "apellido", "email", "contraseña");
 
-    var inputMessage = new Array("nombre", "apellido", "email", "contraseña");
+         $('.error').hide();
 
-     $('.error').hide();
+            if(inputVal[0] == ""){
+                $('#name').after('<span class="error"> Por favor, ingresa tu ' + inputMessage[0] + '</span>');
+            } 
+            else if(!nameReg.test(names)){
+                $('#name').after('<span class="error"> Se aceptan letras solamente</span>');
+            }
 
-        if(inputVal[0] == ""){
-            $('#name').after('<span class="error"> Por favor, ingresa tu ' + inputMessage[0] + '</span>');
-        } 
-        else if(!nameReg.test(names)){
-            $('#name').after('<span class="error"> Se aceptan letras solamente</span>');
-        }
+            if(inputVal[1] == ""){
+                $('#lastname').after('<span class="error"> Por favor, ingresa tu ' + inputMessage[1] + '</span>');
+            }
+            else if(!nameReg.test(lastname)){
+                $('#lastname').after('<span class="error"> Se aceptan letras solamente</span>');
+            }
 
-        if(inputVal[1] == ""){
-            $('#lastname').after('<span class="error"> Por favor, ingresa tu ' + inputMessage[1] + '</span>');
-        }
-        else if(!nameReg.test(lastname)){
-            $('#lastname').after('<span class="error"> Se aceptan letras solamente</span>');
-        }
-
-        if(inputVal[2] == ""){
-            $('#input-email').after('<span class="error"> Por favor, ingresa tu ' + inputMessage[2] + '</span>');
-        } 
-        else if(!emailReg.test(email)){
-            $('#input-email').after('<span class="error"> Por favor, ingresa un email válido.</span>');
-        } 
-    }   
+            if(inputVal[2] == ""){
+                $('#input-email').after('<span class="error"> Por favor, ingresa tu ' + inputMessage[2] + '</span>');
+            } 
+            else if(!emailReg.test(email)){
+                $('#input-email').after('<span class="error"> Por favor, ingresa un email válido.</span>');
+            } 
+        }   
 
 });
