27a1f1aa0fe4dce6f506f0d6fa0dd451b5641ab8
caroquintana <caro.quintana.n@gmail.com>
2017-07-04 16:26:20 -0400

    avance validaciones

--- js/main.js
+++ js/main.js
@@ -1,3 +1,47 @@
+$(document).ready(function(){
+
+$('.submit-btn').click(function(){
+    validateForm();   
+});
+
 function validateForm(){
-	/* Escribe tú código aquí */
-}
+
+
+    var nameReg = /^[A-Za-z]+$/;
+    var emailReg = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
+    var largenumber = ($('#input-password').val()).length >= 6;
+
+    var names = $('#name').val();
+    var lastname = $('#lastname').val();
+    var email = $('#input-email').val();
+    var password = $('#input-password').val();
+
+    var inputVal = new Array(names, lastname, email, password);
+
+    var inputMessage = new Array("nombre", "apellido", "email", "contraseña");
+
+     $('.error').hide();
+
+        if(inputVal[0] == ""){
+            $('#name').after('<span class="error"> Por favor, ingresa tu ' + inputMessage[0] + '</span>');
+        } 
+        else if(!nameReg.test(names)){
+            $('#name').after('<span class="error"> Se aceptan letras solamente</span>');
+        }
+
+        if(inputVal[1] == ""){
+            $('#lastname').after('<span class="error"> Por favor, ingresa tu ' + inputMessage[1] + '</span>');
+        }
+        else if(!nameReg.test(lastname)){
+            $('#lastname').after('<span class="error"> Se aceptan letras solamente</span>');
+        }
+
+        if(inputVal[2] == ""){
+            $('#input-email').after('<span class="error"> Por favor, ingresa tu ' + inputMessage[2] + '</span>');
+        } 
+        else if(!emailReg.test(email)){
+            $('#input-email').after('<span class="error"> Por favor, ingresa un email válido.</span>');
+        } 
+    }   
+
+});
